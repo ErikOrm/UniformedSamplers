6a9864958b74fa75027a9b5d5839d811103f069f
Emilio Jorge <emilio@jorge.se>
2021-02-25 21:12:33 +0100

    Change param

--- act.py
+++ act.py
@@ -16,7 +16,7 @@ def prioAct(params):
         prioleft = []
         for x in crossing.values():
             prioleft.append(np.sum(np.array([len(car.path_list[car.pointer:]) for car in x] <= params.prioleft) + 0.2))
-        weight = np.array([np.exp(x/params.gamma) for x in prioleft])
+        weight = np.array([np.exp(x/params.temp) for x in prioleft])
         weight /= np.sum(weight)
         lighton.append(np.random.choice(crossing.keys,p=weight))
     return lighton
