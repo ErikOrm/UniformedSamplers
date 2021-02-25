786e9f2b50650d637ef81bb285ee185c8d3c7fe0
Emilio Jorge <emilio@jorge.se>
2021-02-25 21:10:50 +0100

    More act

--- act.py
+++ act.py
@@ -1,10 +1,22 @@
 import numpy as np
 
 
-def ThompsonAct(params):
+def thompsonAct(params):
     lighton = []
     for crossing in params.crossings:
         weight = np.array([np.exp(len(x)/params.temp) for x in crossing.values()])
+        weight /= np.sum(weight)
+        lighton.append(np.random.choice(crossing.keys,p=weight))
+    return lighton
+
+
+def prioAct(params):
+    lighton = []
+    for crossing in params.crossings:
+        prioleft = []
+        for x in crossing.values():
+            prioleft.append(np.sum(np.array([len(car.path_list[car.pointer:]) for car in x] <= params.prioleft) + 0.2))
+        weight = np.array([np.exp(x/params.gamma) for x in prioleft])
         weight /= np.sum(weight)
         lighton.append(np.random.choice(crossing.keys,p=weight))
     return lighton
