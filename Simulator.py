21cd6aafbe02028e6c522557cf131b25ca5368d7
Emilio Jorge <emilio@jorge.se>
2021-02-25 20:18:24 +0100

    Add trafficlight

--- Simulator.py
+++ Simulator.py
@@ -20,5 +20,17 @@ class GlobalQueue:
 
 
 class Crossing:
-    def __init__(self, id, ):
+    def __init__(self, street_list):
+        self.crossings = {i:{} for i in range(len(street_list))}
+        for street in street_list:
+            self.crossings[street[1]][street[0]] = []
 
+    def trafficlight(self, lightindex):
+        #lightindex[i] is value of street
+        moved_cars = []
+        for crossing_id, light in enumerate(lightindex):
+            car = self.crossings[crossing_id][light].pop()
+            car.pointer +=1
+            moved_cars.append(car)
+
+        return moved_cars
